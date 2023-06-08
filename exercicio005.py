273c5811b2acbb7554a56dcce48e9cd7f81e2008
Caio César <kayo.ks13@gmail.com>
2023-06-07 21:10:25 -0300

    Create Exercicios 5 e 6
    Exercicios referente a aula 7

---
+++ exercicio005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero: '))
+a = n - 1
+s = n + 1
+
+print(f'Analisando o valor {n}, o seu antecessor é {a} e o sucessor é {s}')
---
+++ exercicio006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+a = n * 2
+b = n * 3
+
+c = n **(1/2)
+
+print(f'o dobro de {n} é {a} \n e o triplo é {b} \n a raiz quadrada é {c:.2f}')
