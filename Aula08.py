9daa04f7840fa50742019587e8aa5e2519497190
Caio César <kayo.ks13@gmail.com>
2023-06-07 22:48:29 -0300

    Create Aula08.py

---
+++ Aula08.py
@@ -0,0 +1,14 @@
+#import math
+#ceil
+#Floor
+#trunc
+#pow
+#sqrt
+#factorial
+# from math import sqrt, floor
+#num = int(input('Digite um numero: '))
+#raiz = sqrt(num)
+#print(f'A raiz de numero {num} é igual a {floor (raiz):.2f}')
+
+import emoji
+print(emoji.emojize("Olá Mundo :eart_americas:", use_aliases=True))
