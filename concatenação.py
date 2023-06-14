05464665c67dc3d5c5c191fd905d8a4d2b7ee452
Caio César <kayo.ks13@gmail.com>
2023-06-13 21:21:28 -0300

    concatenação.py
    Pequena pratica de concatenação acompanhando um post da faculdade.

---
+++ concatenação.py
@@ -0,0 +1,14 @@
+o = "Olá"
+b = " Tudo bem"
+
+a = o + b
+
+print( a )
+
+
+texto = "Aprendendo Python na disciplina de linguagem de programação."
+
+print(f"Tamanho do texto = {len(texto)}")
+print(f"Python in texto = {'Python' in texto}")
+print(f"Quantidade de y no texto = {texto.count('p')}")
+print(f"As 5 primeiras letras são: {texto[0:6]}")
